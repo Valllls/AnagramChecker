b2a991cf05c5786ab6a57b7f9123ff5d98592f03
David <davidfowler1999@gmail.com>
2022-11-09 14:52:18 +0000

    More comments added to better explain code

--- app/src/main/java/anagramchecker/AnagramChecker.java
+++ app/src/main/java/anagramchecker/AnagramChecker.java
@@ -27,25 +27,20 @@ public class AnagramChecker {
         else {
             // Create a hashmap to store the occurances of each character in the strings
             HashMap<Character, Integer> countCharacterOccurances = new HashMap<Character, Integer>();
-            char firstStringCurrentChar = ' ';
-            char secondStringCurrentChar = ' ';
 
             for (int i = 0; i < firstString.length(); i++) {
 
                 if (!Character.isAlphabetic(firstString.charAt(i)) || !Character.isAlphabetic(secondString.charAt(i))) {
                     return false;
                 }
-                firstStringCurrentChar = Character.toLowerCase(firstString.charAt(i));
-                secondStringCurrentChar = Character.toLowerCase(secondString.charAt(i));
-
                 // For each instance of a char in 'firstString' add one to this chars value in
                 // the hashmap 'countCharacterOccurances'.
                 // For each instance of a char in 'secondString' remove one from this chars
                 // value in the hashmap 'countCharacterOccurances'.
-                countCharacterOccurances.put(firstStringCurrentChar,
-                        countCharacterOccurances.getOrDefault(firstStringCurrentChar, 0) + 1);
-                countCharacterOccurances.put(secondStringCurrentChar,
-                        countCharacterOccurances.getOrDefault(secondStringCurrentChar, 0) - 1);
+                countCharacterOccurances.put(Character.toLowerCase(firstString.charAt(i)),
+                        countCharacterOccurances.getOrDefault(Character.toLowerCase(firstString.charAt(i)), 0) + 1);
+                countCharacterOccurances.put(Character.toLowerCase(secondString.charAt(i)),
+                        countCharacterOccurances.getOrDefault(Character.toLowerCase(secondString.charAt(i)), 0) - 1);
             }
 
             // If the strings are anagrams then the occurances of each char should have
@@ -62,13 +57,14 @@ public class AnagramChecker {
 
     /**
      * Reads in the results stored in the results file
-     * Each query from this results file is stored as a UserQuery within an
-     * ArrayList
+     * Each query from this results file is stored as a UserQuery object
+     * within an ArrayList.
      * 
-     * @param filePath The filepath to the anagramResults.txt file.
-     * @return A list of all of the UserQueries within the results file.
+     * @param filePath The filepath to the results file.
+     * @return An ArrayList of the UserQueries read in from the results file.
      */
     public ArrayList<UserQuery> readInResults(String filePath) {
+        System.out.println("Reading in past results from the results file");
         ArrayList<UserQuery> pastResultsList = new ArrayList<UserQuery>();
         String[] temporaryQueryStorage;
         try {
@@ -93,12 +89,13 @@ public class AnagramChecker {
     /**
      * Given a UserQuery this method will write to the results file the information
      * within it. In the format:
-     * username, firstWord, secondWord, isQueryAnAnagram;
+     * \nusername, firstWord, secondWord, isQueryAnAnagram
      * 
-     * @param filePath      The filepath to the anagramResults.txt file.
+     * @param filePath      The filepath to the results file.
      * @param thisUserQuery The UserQuery to be written to the results file
      */
     public void writeToResults(String filePath, UserQuery thisUserQuery) {
+        System.out.println("Writing query result to the results file");
         try {
             BufferedWriter resultsFileWrite = new BufferedWriter(
                     new FileWriter(filePath, true));
@@ -114,34 +111,36 @@ public class AnagramChecker {
     /**
      * This function will loop through the list of already ran queries read in from
      * the results file.
-     * If the query has been run before then 'true' is returned else 'false' is
-     * returned.
-     * If the current query has been run before the isAnagram variable in the class
-     * UserSearch
-     * is set to the value from this already ran query.
+     * If the query is present in the ArrayList of past queries 'true' is returned
+     * if not 'false' is returned.
+     * If the query was previously ran then IsQueryAnAngram within the passed in
+     * UserQuery object is set to the result from this previous query.
      * 
-     * @param pastResultsList Stores an ArrayList of ArrayLists with each ArrayList
-     *                        containing one search query. i.e. username, firstword,
-     *                        secondword and isAnagram.
-     * @param thisUserSearch  An instance of the object UserString, this object
-     *                        stores all the information about the current search.
+     * @param pastResultsList Stores a cache of previously run queries, these are
+     *                        read in from the results file in the readInResults
+     *                        function.
+     * @param thisUserQuery   An instance of the object UserQuery, this object
+     *                        stores all the information about the current query.
      * @return boolean true if currentQuery is within pastResultsList or boolean
      *         false if not.
      */
     public boolean isCurrentTestInHistory(ArrayList<UserQuery> pastResultsList,
             UserQuery thisUserQuery) {
+        System.out.println("Searching the results list for the current Query");
+
         for (UserQuery result : pastResultsList) {
             String thisTest = thisUserQuery.getFirstWord() + thisUserQuery.getSecondWord();
             // If the current 'result' in the pastResultsList has anything but for values it
             // is invalid and should be ignored
-
             if (thisTest.equals(result.getFirstWord() + result.getSecondWord())
                     || thisTest.equals(result.getFirstWord() + result.getSecondWord())) {
                 thisUserQuery.setIsQueryAnAnagram(result.getIsQueryAnAnagram());
+                System.out.println("Current query was found in the list of previous results");
                 return true;
             } else
                 continue;
         }
+        System.out.println("Current query was not found in the list of previous results");
         return false;
     }
 
@@ -149,26 +148,28 @@ public class AnagramChecker {
 
         AnagramChecker myAnagramChecker = new AnagramChecker();
 
+        // Currently this filepath is hardcoded to a set value however in future
+        // versions this could be read in from user input much like the other query
+        // information
         String storedResultsFilePath = System.getProperty("user.dir")
                 + "\\src\\main\\resources\\anagramResults.txt";
 
-        // Create a new instance of UserSearch and populate it with values using
-        // the readQueryInfoFromUser() within UserQuery
+        // Create a new instance of UserQuery and populate it with values using
+        // the readQueryInfoFromUser() function within UserQuery.
         UserQuery thisUserQuery = new UserQuery();
         thisUserQuery.readQueryInfoFromUser();
 
         // Read in previoused stored results from the result file
-        // Store these results in an array of UserSearch
+        // Store these results in an ArrayList of UserQuery
         ArrayList<UserQuery> pastResultsList = myAnagramChecker.readInResults(storedResultsFilePath);
 
         if (myAnagramChecker.isCurrentTestInHistory(pastResultsList, thisUserQuery)) {
             System.out.println("Duplicate Values Found: ");
             myAnagramChecker.writeToResults(storedResultsFilePath, thisUserQuery);
         } else {
-            thisUserQuery
-                    .setIsQueryAnAnagram(
-                            myAnagramChecker.areStringsAnagrams(thisUserQuery.getFirstWord(),
-                                    thisUserQuery.getSecondWord()));
+            thisUserQuery.setIsQueryAnAnagram(
+                    myAnagramChecker.areStringsAnagrams(thisUserQuery.getFirstWord(),
+                            thisUserQuery.getSecondWord()));
             myAnagramChecker.writeToResults(storedResultsFilePath, thisUserQuery);
         }
 
--- app/src/test/java/anagramchecker/Tests.java
+++ app/src/test/java/anagramchecker/Tests.java
@@ -12,12 +12,18 @@ public class Tests {
 
     AnagramChecker myAnagramChecker = new AnagramChecker();
 
+    // This function reads the results from the result list and returns them in the
+    // form of an ArrayList of type UserQuery.
     public ArrayList<UserQuery> beforeListReading() {
         String testResultsFilePathRead = System.getProperty("user.dir")
                 + "\\src\\test\\resources\\testAnagramResultRead.txt";
         return myAnagramChecker.readInResults(testResultsFilePathRead);
     }
 
+    // This function writes a UserQuery to a results file, the modified text file
+    // is then compared to an expected text file. By checking that these two files
+    // are equal the test assures that the writing of results to a file is working
+    // as intended.
     @Test
     public void writingToFile() {
         Path testFile = Paths.get(System.getProperty("user.dir")
@@ -30,8 +36,11 @@ public class Tests {
             // Wipe file
             new PrintWriter(testFile.toString()).close();
 
+            // Write results to file
             myAnagramChecker.writeToResults(testFile.toString(), userQuery1);
 
+            // If the two files contain the same data then Files.mismatch(file1,file2)
+            // will return -1.
             assertEquals(-1, Files.mismatch(groundTruthFile, testFile));
 
         } catch (IOException e) {
@@ -39,51 +48,72 @@ public class Tests {
         }
     }
 
+    // Tests the isCurrentQueryInHistory function to return true when the query does
+    // exist in the ArrayList.
+    // A UserQuery is added to an ArrayList of type UserQuery. This ArrayList is
+    // then searched for this UserQuery with the expectation of it returning true.
     @Test
     public void detectPresenceOfQueryInList() {
         UserQuery userQuery1 = new UserQuery("David", "qwerty", "qwreyt", true);
         ArrayList<UserQuery> pastResultsList = new ArrayList<UserQuery>();
         pastResultsList.add(userQuery1);
-        assertEquals(true, myAnagramChecker.isCurrentTestInHistory(pastResultsList, userQuery1));
+        assertEquals(true, myAnagramChecker.isCurrentQueryInHistory(pastResultsList, userQuery1));
     }
 
+    // Tests the isCurrentQueryInHistory function to return false when the query
+    // does not exist in the ArrayList.
+    // A UserQuery is added to an ArrayList of type UserQuery. This ArrayList is
+    // then searched for a different UserQuery with the expectation of it returning
+    // false.
     @Test
     public void detectLackOfQueryInList() {
         UserQuery userQuery1 = new UserQuery("David", "qwerty", "qwreyt", true);
         UserQuery userQuery2 = new UserQuery("Richard", "zxc", "cvb", false);
         ArrayList<UserQuery> pastResultsList = new ArrayList<UserQuery>();
         pastResultsList.add(userQuery1);
-        assertEquals(false, myAnagramChecker.isCurrentTestInHistory(pastResultsList, userQuery2));
+        assertEquals(false, myAnagramChecker.isCurrentQueryInHistory(pastResultsList, userQuery2));
     }
 
+    // Tests the areStringsAnagrams function to return true if two strings are given
+    // that are anagrams.
     @Test
     public void correctlyFindsAnagram() {
         assertEquals(true, myAnagramChecker.areStringsAnagrams("angered", "enraged"));
     }
 
+    // Tests the areStringsAnagrams function to return false if two strings are
+    // given that are not anagrams.
     @Test
     public void correctlyFindsNoAnagram() {
         assertEquals(false, myAnagramChecker.areStringsAnagrams("angeered", "enraged"));
     }
 
+    // Tests the readInResults method to confirm that the username String is read in
+    // correctly.
     @Test
     public void readUsernameFromResultsFile() {
         ArrayList<UserQuery> tempResultsList = beforeListReading();
         assertEquals("David", tempResultsList.get(0).getUsername());
     }
 
+    // Tests the readInResults method to confirm that the firstWord String is read
+    // in correctly.
     @Test
     public void readFirstWordFromResultsFile() {
         ArrayList<UserQuery> tempResultsList = beforeListReading();
         assertEquals("qwerty", tempResultsList.get(0).getFirstWord());
     }
 
+    // Tests the readInResults method to confirm that the secondWord String is read
+    // in correctly.
     @Test
     public void readSecondWordFromResultsFile() {
         ArrayList<UserQuery> tempResultsList = beforeListReading();
         assertEquals("qwreyt", tempResultsList.get(0).getSecondWord());
     }
 
+    // Tests the readInResults method to confirm that the isQueryAnAnagram boolean
+    // is read in correctly.
     @Test
     public void readFromResultsFile() {
         ArrayList<UserQuery> tempResultsList = beforeListReading();
